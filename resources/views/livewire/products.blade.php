ad676b2e393f6b0349393f0ebcacf9c0d2512bf2
Yuliia Blahoveshchenska <rasskazala@gmail.com>
2026-03-04 13:10:57 +0300

    Added products pagination

--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -7,31 +7,39 @@
 
     <h1 class="text-xl font-bold mb-4">Products</h1>
 
-    @foreach ($products as $product)
+    @forelse ($products as $product)
         <div class="mb-6">
-            <strong>{{ $product['name'] }}</strong><br>
+            <strong>{{ $product->name }}</strong><br>
 
-            Price: {{ $product['price'] }}$<br>
+            Price: {{ number_format($product->price, 2) }}$<br>
 
-            Available: {{ $product['available_quantity'] }}<br>
+            Available: {{ $product->available_quantity }}<br>
 
-            @if ($product['in_cart'] > 0)
+            @if ($product->in_cart > 0)
                 <span class="text-sm text-gray-600">
-                    In cart: {{ $product['in_cart'] }}
+                    In cart: {{ $product->in_cart }}
                 </span><br>
             @endif
 
-            <button wire:click="addToCart({{ $product['id'] }})"
+            <button
+                wire:click="addToCart({{ $product->id }})"
                 class="mt-2 px-3 py-1 border
-           disabled:opacity-50
-           disabled:cursor-not-allowed
-           disabled:bg-gray-100
-           disabled:text-gray-400"
-                @disabled($product['available_quantity'] === 0)>
+                       disabled:opacity-50
+                       disabled:cursor-not-allowed
+                       disabled:bg-gray-100
+                       disabled:text-gray-400"
+                @disabled($product->available_quantity === 0)
+            >
                 Add to cart
             </button>
 
             <hr class="mt-4">
         </div>
-    @endforeach
+    @empty
+        <p>No products found.</p>
+    @endforelse
+
+    <div class="mt-10">
+        {{ $products->links() }}
+    </div>
 </div>
