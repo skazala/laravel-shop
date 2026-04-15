ee3441f97a699759646fc240880963ea5599c71b
Yuliia Blahoveshchenska <rasskazala@gmail.com>
2026-04-15 14:31:41 +0300

    Added Horizon support, updated seeders for product management

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,6 +5,7 @@ return [
     App\Providers\EventServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
